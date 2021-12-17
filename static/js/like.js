2ba1e7be0c348e4755d63741eec872a2a740002a
Vadim <townhor@mail.ru>
2021-12-17 18:37:00 +0500

    Small bugs fixes

--- static/js/like.js
+++ static/js/like.js
@@ -1,6 +1,8 @@
-document.querySelector('.heart').addEventListener('click', function() {
-    this.classList.toggle('is_animating')
+for (var selector of document.querySelectorAll('.heart')) {
+    selector.addEventListener('click', function() {
+        this.classList.toggle('is_animating')
 
-    trackId = this.getAttribute('data-id')
-    window.location.href = `/recs?id=${trackId}`
-})
+        trackId = this.getAttribute('data-id')
+        window.location.href = `/recs?id=${trackId}`
+    })
+}
